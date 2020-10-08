96c8dbd701829238a997c1795f8003662d4762b0
Vasile Ermicioi <vasile.ermicioi@agilepartners.eu>
2020-10-08 22:34:33 +0300

    no need to return headers from function

--- endpoints/index.js
+++ endpoints/index.js
@@ -4,11 +4,6 @@ exports.handler = async (event, context) => {
   try {
     response = {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-      },
       body: JSON.stringify({ path: event.rawPath, headers: event.headers }),
     };
   } catch (err) {
